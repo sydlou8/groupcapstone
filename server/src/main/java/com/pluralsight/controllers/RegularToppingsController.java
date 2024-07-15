214f8a00bf44b3b9b5dd4380fde7c3346e8b2818
Reyna <Reyna221b@outlook.com>
2024-07-15 15:23:06 -0700

    topings/sauces controllers done

--- server/src/main/java/com/pluralsight/controllers/RegularToppingsController.java
+++ server/src/main/java/com/pluralsight/controllers/RegularToppingsController.java
@@ -1,4 +1,34 @@
 package com.pluralsight.controllers;
 
+import com.pluralsight.SandwichModels.RegularToppings;
+import com.pluralsight.data.mysql.MyRegularToppingsDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/regular_toppings")
+@CrossOrigin
 public class RegularToppingsController {
+
+    private MyRegularToppingsDao regularToppingsDao = null;
+
+    @Autowired
+    public RegularToppingsController(MyRegularToppingsDao regularToppingsDao)
+    {
+        this.regularToppingsDao = regularToppingsDao;
+    }
+
+    @GetMapping("")
+    public List<RegularToppings> getAll()
+    {
+        return regularToppingsDao.getAllRegularToppings();
+    }
+
+    @GetMapping("/{id}")
+    public RegularToppings getRegularToppingsById(@PathVariable int id) {
+        return regularToppingsDao.getById(id);
+    }
+
 }
--- server/src/main/java/com/pluralsight/controllers/SaucesController.java
+++ server/src/main/java/com/pluralsight/controllers/SaucesController.java
@@ -1,4 +1,32 @@
 package com.pluralsight.controllers;
 
+import com.pluralsight.data.mysql.MySaucesDao;
+import com.pluralsight.models.toppings.Sauces;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sauces")
 public class SaucesController {
+
+    private MySaucesDao saucesDao;
+
+    public SaucesController(MySaucesDao saucesDao) {
+        this.saucesDao = saucesDao;
+    }
+
+    @GetMapping
+    public List<Sauces> getAllSauces() {
+        return saucesDao.getAllSauces();
+    }
+
+    @GetMapping("/{id}")
+    public Sauces getSauceById(@PathVariable int id) {
+        return saucesDao.getById(id);
+    }
+
 }
