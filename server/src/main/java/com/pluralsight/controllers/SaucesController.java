89763fe015a0f9fb227d6ee6ab67759938088452
Reyna <Reyna221b@outlook.com>
2024-07-15 15:28:04 -0700

    added null exception to sauce/toppings controller

--- server/src/main/java/com/pluralsight/controllers/SaucesController.java
+++ server/src/main/java/com/pluralsight/controllers/SaucesController.java
@@ -1,11 +1,14 @@
 package com.pluralsight.controllers;
 
+import com.pluralsight.data.SaucesDao;
 import com.pluralsight.data.mysql.MySaucesDao;
 import com.pluralsight.models.toppings.Sauces;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,6 +29,11 @@ public class SaucesController {
 
     @GetMapping("/{id}")
     public Sauces getSauceById(@PathVariable int id) {
+        Sauces sauces = saucesDao.getById(id);
+
+        if (sauces == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
         return saucesDao.getById(id);
     }
 
