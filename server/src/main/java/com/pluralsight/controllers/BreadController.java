d09a6647288b86440736b0beb102b3f040409745
venomgto <95735342+venomgto@users.noreply.github.com>
2024-07-15 16:00:54 -0700

    Controllers for bread, cheese and meats

--- server/src/main/java/com/pluralsight/controllers/BreadController.java
+++ server/src/main/java/com/pluralsight/controllers/BreadController.java
@@ -1,4 +1,62 @@
 package com.pluralsight.controllers;
 
-public class BreadController {
+
+import com.pluralsight.SandwichModels.Bread;
+import com.pluralsight.data.BreadDao;
+import com.pluralsight.data.mysql.MyBreadDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/breads")
+public class BreadController
+{
+    private final BreadDao breadDao;
+
+    @Autowired
+    public BreadController(BreadDao breadDao)
+    {
+        this.breadDao = breadDao;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Bread>> getAllBreads()
+    {
+        List<Bread> breads = breadDao.getAllBreads();
+        return ResponseEntity.ok(breads);
+    }
+
+    @GetMapping("/{breadId}")
+    public ResponseEntity<Bread> getBreadById(@PathVariable("breadId") int breadId)
+    {
+        Bread bread = breadDao.getById(breadId);
+        if (bread != null)
+        {
+            return ResponseEntity.ok(bread);
+        } else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Bread>createBread(@RequestBody Bread bread)
+    {
+        Bread createdBread = breadDao.create(bread);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBread);
+    }
+
+    @PutMapping("/{breadId}")
+    public ResponseEntity<Void> updateBread(@PathVariable("breadId") int breadId, @RequestBody Bread bread)
+    {
+        breadDao.update(breadId, bread);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- server/src/main/java/com/pluralsight/controllers/CheeseController.java
+++ server/src/main/java/com/pluralsight/controllers/CheeseController.java
@@ -1,4 +1,69 @@
 package com.pluralsight.controllers;
 
-public class CheeseController {
+
+import com.pluralsight.SandwichModels.Cheese;
+import com.pluralsight.data.CheeseDao;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cheeses")
+public class CheeseController
+{
+    private final CheeseDao cheeseDao;
+
+    public CheeseController(CheeseDao cheeseDao)
+    {
+        this.cheeseDao = cheeseDao;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cheese>> getAllCheeses()
+    {
+        List<Cheese> cheeses = cheeseDao.getAllCheeses();
+
+        return ResponseEntity.ok(cheeses);
+    }
+
+    @GetMapping("/{cheeseId}")
+    public ResponseEntity<Cheese> getCheeseById(@PathVariable int cheeseId)
+    {
+        Cheese cheese = cheeseDao.getById(cheeseId);
+        if (cheese != null)
+        {
+            return ResponseEntity.ok(cheese);
+        } else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Cheese> createCheese(@RequestBody Cheese cheese)
+    {
+        Cheese createdCheese = cheeseDao.create(cheese);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCheese);
+    }
+
+    @PutMapping("/{cheeseId}")
+    public ResponseEntity<Void> updateCheese(@PathVariable int cheeseId, @RequestBody Cheese cheese)
+    {
+        cheeseDao.update(cheeseId, cheese);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{cheeseId}")
+    public ResponseEntity<Void> deleteCheese(@PathVariable int cheeseId)
+    {
+        cheeseDao.delete(cheeseId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
+
