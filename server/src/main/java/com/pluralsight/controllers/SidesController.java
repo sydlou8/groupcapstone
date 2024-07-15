fe39ec48df01c7f1fd217ab9b30bf592e43aa3ed
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-07-15 15:32:29 -0700

    created Sides controllers

--- server/src/main/java/com/pluralsight/controllers/SidesController.java
+++ server/src/main/java/com/pluralsight/controllers/SidesController.java
@@ -1,4 +1,33 @@
 package com.pluralsight.controllers;
 
-public class SidesController {
+
+import com.pluralsight.models.toppings.Side;
+import com.pluralsight.data.mysql.MySidesDao;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sides")
+public class SidesController
+{
+
+    private MySidesDao sidesDao;
+
+    public SidesController(MySidesDao sidesDao)
+    {
+        this.sidesDao = sidesDao;
+    }
+
+    @GetMapping
+    public List<Side> getAllSides()
+    {
+        return sidesDao.getAllSides();
+    }
+
+    @GetMapping("/{id}")
+    public Side getSideById(@PathVariable int id)
+    {
+        return sidesDao.getById(id);
+    }
 }
